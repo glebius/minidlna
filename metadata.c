967931034463ccdd3cffee56b64c7f2db5b3b61a
Gleb Popov <arrowd@FreeBSD.org>
2024-12-28 17:22:22 -0800

    Add support for .ass subtitle format.

--- metadata.c
+++ metadata.c
@@ -146,6 +146,11 @@ check_for_captions(const char *path, int64_t detailID)
 		strcpy(p, ".smi");
 		ret = access(file, R_OK);
 	}
+	if (ret != 0)
+	{
+		strcpy(p, ".ass");
+		ret = access(file, R_OK);
+	}
 
 	if (ret == 0)
 	{
